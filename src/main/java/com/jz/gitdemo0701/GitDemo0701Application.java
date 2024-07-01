fd758c51d329483d2a425e3d519acc6a2d40b4d2
Ariyama11 <3323037801@qq.com>
2024-07-01 12:02:52 +0800

    林磊蠢掉了

---
+++ src/main/java/com/jz/gitdemo0701/GitDemo0701Application.java
@@ -0,0 +1,13 @@
+package com.jz.gitdemo0701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo0701Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemo0701Application.class, args);
+    }
+
+}
